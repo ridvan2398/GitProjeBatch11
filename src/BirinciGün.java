a0ae2a1db1cbf9bea13656f854a0e8e64d9a25f4
unknown <ridvanmahmutince@gmail.com>
2020-11-17 17:24:16 +0100

    ilk commit

---
+++ src/BirinciGün.java
@@ -0,0 +1,11 @@
+
+public class BirinciGün {
+
+	public static void main(String[] args) {
+		System.out.println("11:57 - Birinci Gün");
+		
+
+
+	}
+
+}
---
+++ src/notlar.java
@@ -0,0 +1,43 @@
+
+public class notlar {
+/*
+Git Day 01
+----------
+Git Nedir ?
+	Git bir version kontrol aracıdır. Yaptığımız tüm değişiklikleri, satır satır kaydeder ve istediğimiz zaman eski sürüme dönebilme fırsatı tanır.
+	Örnek : Selamlar.java dosyasında 10. satırı sildik ve onun yerine o satıra "Merhabalar" yazdık.
+	git : sen Selamlar.java dosyasında 10. satırda şöyle bir ifade vardı ve sen onu sildin ve yerine "Merhabalar" yazısını ekledin.
+GitHub Nedir ?
+	Localimizde(kendi bilgisayarımız) projemizle alakalı yaptığımız tüm değişiklikler(yeni sürümler, proje dosyaları, değişiklik geçmişi vs.) uzak bir sunucuda saklanır. Biz bu uzak sunucuya github.com diyoruz.
+GitHub'ın faydaları nelerdir ?
+	*Kodlarımız güvende olur.
+	*Senkronize çalışma fırsatı.
+	*Kolay erişim.
+	*Projemizi kolayca store etme.
+	*Zaman tasarrufu.
+	*Hafıza tasarrufu.
+	*Değişikliklerin takımdaki tüm üyeler tarafından kolayca görünmesi
+	***** tüm takımın bir proje üzerinde çalışabilmesini sağlıyor.
+	***** dataların birleştirilmesi
+	***** bilgisayara gerek duymadan, uzak sunucudaki kodları çalıştırabiliyoruz.
+---------------------------    Git komutları    --------------------
+***** projemizdeki dosyaları ilk önce STAGE AREA' ya daha sonra da STAGE AREA'daki dosyaları LOCAL REPOSITORY'ye ekleyeceğiz.
+git init  -> herbir proje için sadece 1 kere kullanılıyor.
+			 local repository oluşturuyor.
+git add . -> projemizdeki tüm dosyaları STAGE AREA'ya ekliyor.
+git commit -m "mesajı yazıyor" -> STAGE AREA'dan LOCAL REPOSITORY'ye taşıyor. Biz bu işleme KİMLİK(commit) KAZANDIRMA İŞLEMİ diyoruz.	 
+git push  -> bu komut yardımıyla local repository'deki dosyalar, github.com'a gönderiliyor.
+git log   -> commit geçmişini gösteriyor
+git show (commitin ilk 5 karakteri) -> commit ile ilgili detaylı bilgiler
+---------------------------Basit (cmd) komutları--------------------
+cd    -> cd klasorAdresi ya da cd KlasorIsmi şeklinde kullanılır.
+		 herhangi bir klasörün içerisine gitmek içn kullanılır
+cd .. -> bir üst klasöre geri dönüyoruz.
+		 Örnek :  C:/Users/İsimsiz     cd ..
+		 		  C:/Users
+pwd   -> şuanda bulunduğumuz klasörün adresini bize gösteriyor.
+dir   -> bulunduğumuz klasörün içerisindeki tüm dosyaları ayrıntılı bir şekilde gösteriyor.
+ls    -> bulunduğumuz klasörün içerisindeki tüm dosyaları sadece isimleriyle gösteriyor.(ayrıntılı değil)
+cls   -> cmd ekranını temizliyor.
+ */
+}
