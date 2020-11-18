ec261a73465992efbdc2297f7e6dfc36f465357d
unknown <ridvanmahmutince@gmail.com>
2020-11-18 15:14:04 +0100

    mesaj

--- src/BirinciGün.java
+++ src/BirinciGün.java
@@ -3,7 +3,7 @@ public class BirinciGün {
 
 	public static void main(String[] args) {
 		System.out.println("11:57 - Birinci Gün");
-		System.out.println("selam kardeş ");
+		System.out.println("selam kardeş");
 
 
 	}
